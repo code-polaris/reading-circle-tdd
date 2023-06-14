9d649c3d9cb286d0020f517c8b3f282d89f186a1
shino51 <shino.takeshita1124@gmail.com>
2023-06-14 14:22:17 +0200

    Added java starter setting

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationTest {
+
+  private final Application application = new Application();
+
+  @Test
+  public void dummyTest() {
+    // dummy test
+  }
+}
