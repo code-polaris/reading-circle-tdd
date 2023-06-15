ed24bba4083b1759c3037fbcd01d10fb40b30bc9
shino51 <shino.takeshita1124@gmail.com>
2023-06-15 14:39:29 +0200

    Added missing package and updated the path to sonar way

--- src/main/java/org/codepolaris/tdd/Application.java
+++ src/main/java/org/codepolaris/tdd/Application.java
@@ -1,5 +1,7 @@
+package org.codepolaris.tdd;
+
 public class Application {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    // main here
   }
 }
--- src/test/java/org/codepolaris/tdd/ApplicationTest.java
+++ src/test/java/org/codepolaris/tdd/ApplicationTest.java
@@ -1,12 +1,14 @@
+package org.codepolaris.tdd;
+
 import org.junit.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ApplicationTest {
 
-  private final Application application = new Application();
-
   @Test
   public void dummyTest() {
-    // dummy test
+    boolean isTrue = true;
+    assertThat(isTrue).isTrue();
   }
 }
