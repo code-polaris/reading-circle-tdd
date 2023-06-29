51ba8eee5ecb3a841dc27ff08c6ff59ee335f649
shino51 <shino.takeshita1124@gmail.com>
2023-06-29 14:25:10 +0200

    #17 自分の名前付きのフォルダーの移動と、不要なコメントや後々実装予定の先取りコードを削除した

--- shino/src/main/java/org/codepolaris/tdd/Dollar.java
+++ shino/src/main/java/org/codepolaris/tdd/Dollar.java
@@ -13,10 +13,6 @@ public class Dollar {
     return new Dollar(amount * multiplier);
   }
 
-  /**
-   * According to the book, equals method cast object directly, but this isn't good java
-   * practice. In this method equals method is followed on the Java's manner.
-   */
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -25,10 +21,6 @@ public class Dollar {
     return amount == dollar.amount;
   }
 
-  /**
-   * According to the book, the class doesn't have the hashCode method. But this has to be implemented when
-   * equals methods is override. So it's been added here.
-   */
   @Override
   public int hashCode() {
     return Objects.hash(amount);
