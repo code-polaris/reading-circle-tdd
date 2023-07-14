892a5f5f319e628be1f40b23999755e15bee9f6c
kalen <kalen573@gmail.com>
2023-07-14 10:29:01 +0900

    新しいテストを追加

--- kalen/test_money.py
+++ kalen/test_money.py
@@ -2,7 +2,6 @@ from dollar import Dollar
 
 class TestMoney:
     def test_multiplication(self):
-        
         five = Dollar(5)
         assert five.times(2) == Dollar(10)
         assert five.times(3) == Dollar(15)
@@ -10,4 +9,10 @@ class TestMoney:
     def test_Equality(self):
         assert Dollar(5) == Dollar(5)
         assert not Dollar(5) == Dollar(6)
+
+    def test_FrancMultiplication(self):
+        five = Franc(5)
+        assert five.times(2) == Franc(10)
+        assert five.times(3) == Franc(15)
+
 
