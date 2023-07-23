2c3a5ba33d3c5d8012ed86b48d8cd5e9199e7e4a
Maho Miyata <mahomiyata@users.noreply.github.com>
2023-07-23 21:52:02 +0900

    improve tests & use private for amount (chapter 4)

--- miyata/miyata-reading-circle-tdd/src/main/java/money/Dollar.java
+++ miyata/miyata-reading-circle-tdd/src/main/java/money/Dollar.java
@@ -2,7 +2,7 @@ package money;
 
 public class Dollar {
 
-    int amount;
+    private int amount;
     Dollar(int amount) {
         this.amount = amount;
     }
--- miyata/miyata-reading-circle-tdd/src/test/java/money/MoneyTest.java
+++ miyata/miyata-reading-circle-tdd/src/test/java/money/MoneyTest.java
@@ -3,18 +3,16 @@ package money;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MoneyTest {
+class MoneyTest {
     @Test
-    public void testMultiplication() {
+    void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
-    public void testEquality() {
+    void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertFalse(new Dollar(5).equals(new Object()));
