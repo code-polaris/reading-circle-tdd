bb505e860b250913c3d77c695dc790de68ef441b
shino51 <shino.takeshita1124@gmail.com>
2023-07-25 13:41:12 +0200

    #25 罪深いコピペによるFrancの実装をした

--- shino/src/test/java/org/codepolaris/tdd/MoneyTest.java
+++ shino/src/test/java/org/codepolaris/tdd/MoneyTest.java
@@ -18,4 +18,11 @@ class MoneyTest {
     assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
     assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
   }
+
+  @Test
+  void testFrancMultiplication() {
+    Franc five = new Franc(5);
+    assertThat(five.times(2)).isEqualTo(new Franc(10));
+    assertThat(five.times(3)).isEqualTo(new Franc(15));
+  }
 }
