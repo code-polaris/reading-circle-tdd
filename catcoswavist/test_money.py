d2c51f313980c07f4a140efe3366396cb6d4631f
catcoswavist <mimosa.29.mint@gmail.com>
2023-07-27 21:08:55 +0900

    #15 第1章 p.30までの仮実装Dollar.py、test_money.py、TODO.md作成

---
+++ catcoswavist/test_money.py
@@ -0,0 +1,10 @@
+import pytest
+#testの構造: test_*で始まるを選択したため、test_*.pyで作成
+from dollar import Dollar
+
+class TestMoney():
+    def test_multiplication(self):
+        
+        five = Dollar(5)
+        product = five.times(2)
+        assert 10 == five.amount
