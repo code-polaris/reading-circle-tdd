0af495f987a7af6847085ed80c96c631e00d6e32
catcoswavist <mimosa.29.mint@gmail.com>
2023-08-03 18:59:41 +0900

    #16 第2章 p.44まで完了 副作用解消

--- catcoswavist/dollar.py
+++ catcoswavist/dollar.py
@@ -2,5 +2,14 @@ class Dollar:
     def __init__(self,amount:int) -> int:
         self.amount = amount
 
-    def times(self, multiplier:int) -> int:
-        self.amount *= multiplier
+    def times(self, multiplier:int):
+        print("amount",self.amount)
+        return Dollar(self.amount*multiplier)
+    # どうやったらテスト通るか？第2章読み直し
+    # なぜintじゃなくてDollarにしたか？
+    
+    # ->前のコードでは、最初のtimes呼び出しで5ドルが10ドルになる(再帰)
+    # -> timesの呼び出し時には掛け算をせず、
+    # returnするときだけ掛け算をすることで再帰回避
+    # 【疑問】テストは通ったが、出力が見えない。
+    # python .\test_money.py > output.txtしたのに白紙。
--- catcoswavist/test_money.py
+++ catcoswavist/test_money.py
@@ -6,5 +6,12 @@ class TestMoney():
     def test_multiplication(self):
         
         five = Dollar(5)
-        five.times(2)
-        assert 10 == five.amount
+        product = five.times(2)
+        assert 10 == product.amount
+        # この段階のproductはtostringを通していないのでただのアドレス(読めない)
+        print("プロダクト10",product.amount)
+        product = five.times(3)
+        assert 15 == product.amount
+        print("プロダクト15",product.amount)
+
+#TestMoney().test_multiplication()
