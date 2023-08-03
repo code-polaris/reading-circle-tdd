11001e53cf16a949712cca8878d2d30afe6041a8
catcoswavist <mimosa.29.mint@gmail.com>
2023-08-03 21:21:34 +0900

    #16 第2章 実行時にテストの結果を出力できるようにした

--- catcoswavist/test_money.py
+++ catcoswavist/test_money.py
@@ -2,7 +2,10 @@ import pytest
 #testの構造: test_*で始まるを選択したため、test_*.pyで作成
 from dollar import Dollar
 
-class TestMoney():
+class TestMoney:
+    # class指定は()いらない、オブジェクト指定痔に()必要。
+    # があると()インスタンスができる
+    # ()がいらないクラス指定→staticメソッド
     def test_multiplication(self):
         
         five = Dollar(5)
@@ -14,4 +17,4 @@ class TestMoney():
         assert 15 == product.amount
         print("プロダクト15",product.amount)
 
-#TestMoney().test_multiplication()
+TestMoney().test_multiplication()
