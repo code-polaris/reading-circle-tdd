3e18ad110f936a1ee4ad7065365946e768b57bfe
Maho Miyata <mahomiyata@users.noreply.github.com>
2023-08-03 22:09:53 +0900

    Fixed a misspelling

---
+++ miyata/miyata-reading-circle-tdd/src/main/java/money/Franc.java
@@ -0,0 +1,11 @@
+package money;
+
+public class Franc extends Money {
+    Franc(int amount) {
+        this.amount = amount;
+    }
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+}
--- miyata/miyata-reading-circle-tdd/src/test/java/money/MoneyTest.java
+++ miyata/miyata-reading-circle-tdd/src/test/java/money/MoneyTest.java
@@ -16,15 +16,15 @@ class MoneyTest {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertFalse(new Dollar(5).equals(new Object()));
-        assertTrue(new Fran(5).equals(new Fran(5)));
-        assertFalse(new Fran(5).equals(new Fran(6)));
-        assertFalse(new Fran(5).equals(new Object()));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Object()));
     }
 
     @Test
     void testFranMultiplication() {
-        Fran five = new Fran(5);
-        assertEquals(new Fran(10), five.times(2));
-        assertEquals(new Fran(15), five.times(3));
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
