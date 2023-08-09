6136fc4011c9f107918577fea3107b6d64a441ea
catcoswavist <mimosa.29.mint@gmail.com>
2023-08-09 21:25:42 +0900

    #26 第6章 p.74まで Dollar.pyでの一時変数をDollarクラスからMoneyクラスに変更

--- catcoswavist/dollar.py
+++ catcoswavist/dollar.py
@@ -3,20 +3,18 @@ from money import Money
 # ↑ kanen573さんの拝見(https://github.com/code-polaris/reading-circle-tdd/commit/6643ed6dd5e11cd29f3a7b0923bb15c8de2bf6e7)
 class Dollar(Money):
     def __init__(self,amount:int) -> int:
+        # サブクラスで__init__を定義すると、親の__init__が上書きされてしまうためsuper()を使用？(参照:https://commte.net/7413)
+        # これがないと`TypeError: Money.__init__() takes 1 positional argument but 2 were given`のエラー？
+        super().__init__(amount)
         # サブクラスから見えるように知るため、amount関数をprivateからprotectedに変更
         # Pythonにprotectedは存在しない → self.__amountのままでいい？
         self.__amount = amount
-        # 【疑問】kalen573さんの拝見 デコレータ@propertyをなぜ使うか疑問
-        # 【疑問】アンダースコア(_)1個？2個？
- 
-    @property
-    def amount(self):
-        return self.__amount
 
     def times(self, multiplier:int):
         return Dollar(self.__amount*multiplier)
+        # まだMoneyに変換しない、Dollarのまま(変更するとAttributeError: 'Money' object has no attribute 'equals'になった)
     
-    def equals(self,other):
-        dollar = other
-        return self.__amount == dollar.amount
+    def equals(self,object: Money) -> bool:
+        money = object
+        return self.__amount == money.amount
 
--- catcoswavist/money.py
+++ catcoswavist/money.py
@@ -1,3 +1,8 @@
 class Money:
-    def __init__(self):
-        self.__amount = 0
+    def __init__(self,amount):
+        self.__amount = amount
+    @property
+    def amount(self):
+        return self.__amount
+        # 【疑問】kalen573さんの拝見 デコレータ@propertyをなぜ使うか疑問
+        # 【疑問】アンダースコア(_)1個？2個？
