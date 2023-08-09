9095d067018968a9e4253e4de4c2f191d681a397
catcoswavist <mimosa.29.mint@gmail.com>
2023-08-09 21:32:36 +0900

    #26 第6章 p.75まで equalsメソッドをDollarクラスからMoneyクラスに引き上げ

--- catcoswavist/money.py
+++ catcoswavist/money.py
@@ -6,3 +6,8 @@ class Money:
         return self.__amount
         # 【疑問】kalen573さんの拝見 デコレータ@propertyをなぜ使うか疑問
         # 【疑問】アンダースコア(_)1個？2個？
+
+    def equals(self,object) -> bool:
+        money = object
+        return self.__amount == money.amount
+        # __amountのアンダースコア外す？外さない？
