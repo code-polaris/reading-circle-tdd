ff37d316d0734943521719b102a03de022f60a93
kalen <kalen573@gmail.com>
2023-08-18 10:55:13 +0900

    currencyメソッドを追加

--- kalen/money.py
+++ kalen/money.py
@@ -1,24 +1,28 @@
 from abc import abstractmethod
 
 class Money:
-    def __init__(self, amount):
+    def __init__(self, amount, currency):
         self.__amount = amount
+        self.__currency = currency
     
     def __eq__(self, object) -> bool:
         money = object
         return self.amount == money.amount and type(self) == type(money)
+    # currencyメソッドを追加。インスタンス変数も追加
+    def currency(self):
+        return self.__currency
 
     @property
     def amount(self):
         return self.__amount
-
+    # staticの戻り値に通貨を追加
     @staticmethod
     def dollar(amount: int):
-        return Dollar(amount)
-    # staticmethodでFrancを設置
+        return Dollar(amount, "USD")
+    
     @staticmethod
     def franc(amount: int):
-        return Franc(amount)
+        return Franc(amount, "CHF")
     
     @abstractmethod
     def times(self, multiplier):
