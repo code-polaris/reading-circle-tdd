5052f1ac41d26a570c2174ec9f44fcf2a94a2c7f
kalen <kalen573@gmail.com>
2023-08-18 10:17:22 +0900

    moneyに通貨比較のコードを追加　第7章完了

--- kalen/money.py
+++ kalen/money.py
@@ -7,4 +7,8 @@ class Money:
     
     def __eq__(self, object) -> bool:
         money = object
-        return self.amount == money.amount
+        return self.amount == money.amount and type(self) == type(money)
+    # and type(self) == type(money) この部分がテストでフランとダラーを比べるとき、
+    # 同じ通貨同士の時のみ等価比較させるようにする部分
+    # これでテストは通ります。
+    # これが第7章の変更部分
