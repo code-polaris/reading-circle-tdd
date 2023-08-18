afc1bab5fd1761c83ff8075390af44a15fbe268c
kalen <kalen573@gmail.com>
2023-08-18 10:38:47 +0900

    コードを整理

--- kalen/money.py
+++ kalen/money.py
@@ -3,16 +3,15 @@ from abc import abstractmethod
 class Money:
     def __init__(self, amount):
         self.__amount = amount
-    @property
-    def amount(self):
-        return self.__amount
     
     def __eq__(self, object) -> bool:
         money = object
         return self.amount == money.amount and type(self) == type(money)
 
-    # 静的メソッドを追加。この時クラスのインポートがループするためMoneyクラスファイルにサブクラスも置くようにする
-    # classmethodにしない理由は、dollarメソッドが特にクラスのバックグラウンドを必要としないため。
+    @property
+    def amount(self):
+        return self.__amount
+
     @staticmethod
     def dollar(amount: int):
         return Dollar(amount)
@@ -29,6 +28,5 @@ class Dollar(Money):
         super().__init__(amount)
         self.__amount = amount
     
-    # フランとダラーのタイムズメソッドを一致させMoneyに置けるようにする
     def times(self, multiplier: int):
         return Dollar(self.amount * multiplier)
