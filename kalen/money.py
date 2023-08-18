7572b794099b1b3e37dab4a35cb75c0d5153c969
kalen <kalen573@gmail.com>
2023-08-18 10:32:04 +0900

    テストの書き換え。Moneyクラスにstaticmethodを追加

--- kalen/money.py
+++ kalen/money.py
@@ -8,7 +8,20 @@ class Money:
     def __eq__(self, object) -> bool:
         money = object
         return self.amount == money.amount and type(self) == type(money)
-    # and type(self) == type(money) この部分がテストでフランとダラーを比べるとき、
-    # 同じ通貨同士の時のみ等価比較させるようにする部分
-    # これでテストは通ります。
-    # これが第7章の変更部分
+
+    # 静的メソッドを追加。この時クラスのインポートがループするためMoneyクラスファイルにサブクラスも置くようにする
+    # classmethodにしない理由は、dollarメソッドが特にクラスのバックグラウンドを必要としないため。
+    @staticmethod
+    def dollar(amount: int):
+        return Dollar(amount)
+    
+    # -----------------
+    
+class Dollar(Money):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.__amount = amount
+    
+    # フランとダラーのタイムズメソッドを一致させMoneyに置けるようにする
+    def times(self, multiplier: int):
+        return Dollar(self.amount * multiplier)
--- kalen/test_money.py
+++ kalen/test_money.py
@@ -1,15 +1,17 @@
 from dollar import Dollar
 from franc import Franc
+from money import Money
 
 class TestMoney:
     def test_multiplication(self):
-        five = Dollar(5)
-        assert five.times(2) == Dollar(10)
-        assert five.times(3) == Dollar(15)
+        # MoneyからDollarを返すように書き換える。Moneyクラスにstaticメソッドを設置する
+        five = Money.dollar(5)
+        assert five.times(2) == Money.dollar(10)
+        assert five.times(3) == Money.dollar(15)
 
     def test_Equality(self):
-        assert Dollar(5) == Dollar(5)
-        assert not Dollar(5) == Dollar(6)
+        assert Money.dollar(5) == Money.dollar(5)
+        assert not Money.dollar(5) == Money.dollar(6)
         assert Franc(5) == Franc(5)
         assert not Franc(5) == Franc(6)
 
@@ -17,10 +19,7 @@ class TestMoney:
         five = Franc(5)
         assert five.times(2) == Franc(10)
         assert five.times(3) == Franc(15)
-        # 新しいテストを追加
-        assert not Franc(5) == Dollar(5)
-        # この状態だとエラーになる、つまりフランとダラーは正しいことになっているが、それはおかしい。
-        # 今は通貨概念をまだ導入していないので、Moneyクラスで実クラスが等しい時のみ等価比較を行うことにする
+        assert not Franc(5) == Money.dollar(5)
 
 
 
