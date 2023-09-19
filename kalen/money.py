ed6f589791276f6655de1f5c9240653750b29156
kalen <kalen573@gmail.com>
2023-09-19 16:37:21 +0900

    不要になったテストとFrancクラスを削除　第11章完了 #31

--- kalen/money.py
+++ kalen/money.py
@@ -6,7 +6,6 @@ class Money:
         self.__currency = currency
     
     def __eq__(self, object) -> bool:
-        #money = object
         return self.amount == object.amount and self.currency == object.currency
 
     @property
@@ -20,23 +19,10 @@ class Money:
     @staticmethod
     def dollar(amount: int):
         return Money(amount, "USD")
-    # サブクラスを削除する準備としてMoneyクラスを返すようにする
+
     @staticmethod
     def franc(amount: int):
         return Money(amount, "CHF")
 
     def times(self, multiplier):
         return Money(self.amount * multiplier, self.currency)
-        #pass
-    
-    # -----------------
-# Dollarクラスを参照しているテストはないのでDollarクラスは削除する        
-# class Dollar(Money):
-#     def __init__(self, amount, currency):
-#         super().__init__(amount, currency)
-   
-    # -----------------
-    
-class Franc(Money):
-    def __init__(self, amount, currency):
-        super().__init__(amount, currency)
--- kalen/test_money.py
+++ kalen/test_money.py
@@ -1,5 +1,3 @@
-from dollar import Dollar
-from franc import Franc
 from money import Money
 
 class TestMoney:
@@ -11,20 +9,9 @@ class TestMoney:
     def test_Equality(self):
         assert Money.dollar(5) == Money.dollar(5)
         assert not Money.dollar(5) == Money.dollar(6)
-        assert Money.franc(5) == Money.franc(5)
-        assert not Money.franc(5) == Money.franc(6)
-
-    def test_FrancMultiplication(self):
-        five = Money.franc(5)
-        assert five.times(2) == Money.franc(10)
-        assert five.times(3) == Money.franc(15)
         assert not Money.franc(5) == Money.dollar(5)
-
+    
     def test_Currency(self):
         assert Money.dollar(1).currency == "USD"
         assert Money.franc(1).currency == "CHF"
-# 新しいテストを追加
-    #def test_DifferentClassEquality(self):
-        #assert Money(10, "CHF") == Franc(10, "CHF")
-
 
