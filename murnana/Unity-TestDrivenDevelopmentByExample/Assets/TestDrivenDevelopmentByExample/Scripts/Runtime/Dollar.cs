f38530606a3eda90a626d6b9cf425c019f89da55
murnana <murnana@users.noreply.github.com>
2023-10-16 08:39:08 +0900

    :sparkles: 第10章 テストに聞いてみる
    - `Dollar.Times` が `Money`を返却するように変更

--- murnana/Unity-TestDrivenDevelopmentByExample/Assets/TestDrivenDevelopmentByExample/Scripts/Runtime/Dollar.cs
+++ murnana/Unity-TestDrivenDevelopmentByExample/Assets/TestDrivenDevelopmentByExample/Scripts/Runtime/Dollar.cs
@@ -9,7 +9,7 @@ namespace TDD
 
         public override Money Times(int multiplier)
         {
-            return new Dollar (amount: m_Amount * multiplier, currency: m_Currency);
+            return new Money (amount: m_Amount * multiplier, currency: m_Currency);
         }
     }
 }
