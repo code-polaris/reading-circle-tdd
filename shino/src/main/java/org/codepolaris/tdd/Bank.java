889408441bd40066b873700dc3a3ec0bdcbb9e17
shino51 <shino.takeshita1124@gmail.com>
2023-10-27 16:34:17 +0200

    #60 第16章 将来の読み手を考えたテスト timesをExpressionにプロモーションし、テストのBeforeEachも導入した

--- shino/src/main/java/org/codepolaris/tdd/Bank.java
+++ shino/src/main/java/org/codepolaris/tdd/Bank.java
@@ -1,13 +1,12 @@
 package org.codepolaris.tdd;
 
-import org.codepolaris.tdd.utils.Currency;
 import org.codepolaris.tdd.utils.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Bank {
-  private Map<Pair, Integer> rates = new HashMap<>();
+  private final Map<Pair, Integer> rates = new HashMap<>();
 
   public Money reduce(Expression source, String to) {
     return source.reduce(this, to);
