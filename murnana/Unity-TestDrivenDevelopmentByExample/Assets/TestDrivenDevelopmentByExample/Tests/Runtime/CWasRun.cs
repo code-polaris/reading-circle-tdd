4d9095b011415ee7221f6897f1eb764831c1b2a3
murnana <murnana@users.noreply.github.com>
2023-12-17 11:13:32 +0900

    :sparkles: 第18章 xUnitへ向かう小さな一歩
    - 通常のだと芸がないので、Reflectionを追加

---
+++ murnana/Unity-TestDrivenDevelopmentByExample/Assets/TestDrivenDevelopmentByExample/Tests/Runtime/CWasRun.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+namespace TDD.Tests
+{
+    /// <summary>
+    /// 第18章「xUnitへ向かう小さな一歩」のサンプル
+    /// </summary>
+    internal sealed class CWasRun
+    {
+        public CWasRun(string name)
+        {
+            WasRun = null;
+        }
+
+        public int? WasRun { get; private set; }
+
+        public void TestMethod()
+        {
+            WasRun = 1;
+        }
+    }
+}
---
+++ murnana/Unity-TestDrivenDevelopmentByExample/Assets/TestDrivenDevelopmentByExample/Tests/Runtime/NUnitSample.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+using System;
+using NUnit.Framework;
+
+namespace TDD.Tests
+{
+    [Description("第18章「xUnitへ向かう小さな一歩」のサンプル")]
+    public sealed class NUnitSample
+    {
+        [Test]
+        [Description("C#な書き方")]
+        public void Sample()
+        {
+            var test = new CWasRun("TestMethod");
+            TestContext.WriteLine(test.WasRun);
+            test.TestMethod();
+            TestContext.WriteLine(test.WasRun);
+        }
+
+        [Test]
+        [Description("Reflectionを使ったC#の書き方(IL2CPPだとアウト)")]
+        public void SampleReflection()
+        {
+            var testType = typeof(CWasRun);
+            var test     = Activator.CreateInstance(type: testType, "TestMethod");
+
+            var wasRun     = testType.GetProperty("WasRun");
+            var testMethod = testType.GetMethod("TestMethod");
+
+            // print(test.wasRun)
+            TestContext.WriteLine(wasRun?.GetValue(test));
+
+            // test.testMethod
+            testMethod?.Invoke(obj: test, parameters: null);
+
+            // print(test.wasRun)
+            TestContext.WriteLine(wasRun?.GetValue(test));
+        }
+    }
+}
