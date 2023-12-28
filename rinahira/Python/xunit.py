677451ec01de40ae010107be20ccb1664eb48335
rinahira <monofrontier@gmail.com>
2023-12-28 14:17:16 +0900

    第18章 xUnitへ向かう小さな一歩

---
+++ rinahira/Python/xunit.py
@@ -0,0 +1,23 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        super().__init__(name)
+    def testMethod(self):
+        self.wasRun = 1
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
+
