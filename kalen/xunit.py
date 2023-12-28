2560542d559c9652b0cfa537ab896826aee5c313
kalen <kalen573@gmail.com>
2023-12-28 10:15:28 +0900

    Runメソッドの追加

--- kalen/xunit.py
+++ kalen/xunit.py
@@ -1,12 +1,15 @@
-# 実行時メソッドが足りないと怒られていたメソッドを追加
+# Runメソッドを追加してそこから呼び出す仕様に変更
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+
+    def run(self):
+        self.testMethod()
 
     def testMethod(self):
         self.wasRun = 1
 
 test = WasRun("testMethod")
 print(test.wasRun)
-test.testMethod()
+test.run()
 print(test.wasRun)
