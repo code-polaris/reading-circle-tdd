458bb30b18f819e204fd70ee164c25b78f792534
rinahira <monofrontier@gmail.com>
2023-12-28 14:43:31 +0900

    第19章 前準備

--- rinahira/Python/xunit.py
+++ rinahira/Python/xunit.py
@@ -1,23 +1,30 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name):
+    def setUp(self):
         self.wasRun = None
-        super().__init__(name)
+        self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
