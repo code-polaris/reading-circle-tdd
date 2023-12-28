4b8468aac2a25222cd5dfabecfe7adfde363f3e2
rinahira <monofrontier@gmail.com>
2023-12-28 16:39:35 +0900

    第20章 後片付け

--- rinahira/Python/xunit.py
+++ rinahira/Python/xunit.py
@@ -3,28 +3,30 @@ class TestCase:
         self.name = name
     def setUp(self):
         pass
+    def tearDown(self):
+        pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
-class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+class TestCaseTest(TestCase):   
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+
+TestCaseTest("testTemplateMethod").run()
 
