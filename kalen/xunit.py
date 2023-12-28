e1fb9e59f4ff728afb297109ad39051aaed95bc1
kalen <kalen573@gmail.com>
2023-12-28 11:06:36 +0900

    初期化処理をsetUpに任せる　テストをシンプル化　第19章完了 #70

--- kalen/xunit.py
+++ kalen/xunit.py
@@ -11,28 +11,27 @@ class TestCase:
         method = getattr(self, self.name)
         method()
 
-class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
-    
+class WasRun(TestCase):    
     def setUp(self):
+        # wasRunフラグを移動　コンストラクタは削除
+        # これでsetUp起動のたびにwasRunが初期化される
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
-
+# テストをシンプルに書き直す
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+        
     def TestRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
-    # 新しいテストを追加
+        self.test.run()
+        assert self.test.wasRun
+    
     def TestSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert test.wasSetUp
+        self.test.run()
+        assert self.test.wasSetUp
         
 TestCaseTest("TestRunning").run()
 TestCaseTest("TestSetUp").run()
