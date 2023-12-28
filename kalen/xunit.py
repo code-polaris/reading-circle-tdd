626da859cefa07a59981f7b0cd4797a9d1abeda6
kalen <kalen573@gmail.com>
2023-12-28 11:12:21 +0900

    記録用のログを見れるようにする

--- kalen/xunit.py
+++ kalen/xunit.py
@@ -1,37 +1,37 @@
 class TestCase:
     def __init__(self, name):
         self.name= name
-    # setupメソッドを設定
+
     def setUp(self):
         pass
 
     def run(self):
-        # setUpを呼ぶ責務はTestCaseにこそあるべき
         self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):    
     def setUp(self):
-        # wasRunフラグを移動　コンストラクタは削除
-        # これでsetUp起動のたびにwasRunが初期化される
         self.wasRun = None
         self.wasSetUp = 1
+        # 記録用のログを保持できるようにする
+        self.log = "setUp"
 
     def testMethod(self):
         self.wasRun = 1
-# テストをシンプルに書き直す
+
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
-        
+
     def TestRunning(self):
         self.test.run()
         assert self.test.wasRun
     
     def TestSetUp(self):
         self.test.run()
-        assert self.test.wasSetUp
+        # ログを見るテストに変更
+        assert "setUp" == self.test.log
         
 TestCaseTest("TestRunning").run()
 TestCaseTest("TestSetUp").run()
