2987d0fd41acc3c6be193ec0f858458d0246f701
kalen <kalen573@gmail.com>
2023-12-28 10:46:30 +0900

    setUpメソッドを追加

--- kalen/xunit.py
+++ kalen/xunit.py
@@ -1,8 +1,13 @@
 class TestCase:
     def __init__(self, name):
         self.name= name
-    # runメソッドを引き上げ
+    # setupメソッドを設定
+    def setUp(self):
+        pass
+
     def run(self):
+        # setUpを呼ぶ責務はTestCaseにこそあるべき
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -10,16 +15,24 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
+    
+    def setUp(self):
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
 
-# 「テストのようなもの」をテストフレームワークに昇華
 class TestCaseTest(TestCase):
     def TestRunning(self):
         test = WasRun("testMethod")
         assert not test.wasRun
         test.run()
         assert test.wasRun
+    # 新しいテストを追加
+    def TestSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert test.wasSetUp
         
 TestCaseTest("TestRunning").run()
+TestCaseTest("TestSetUp").run()
