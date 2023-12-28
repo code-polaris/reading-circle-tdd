20434c70562b0691f0faa3399947312b2aa37980
kalen <kalen573@gmail.com>
2023-12-28 11:39:37 +0900

    新たなテストを追加

--- kalen/xunit.py
+++ kalen/xunit.py
@@ -29,5 +29,10 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert  test.log ==  "setUp testMethod tearDown"
+    # 新たなテストを追加
+    def TestResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert result.summary() == "1 run, 0 failed"
         
 TestCaseTest("TestTemplateMethod").run()
