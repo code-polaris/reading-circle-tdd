f93c5fd491c3205d8229c990a2f4cadd542b0776
shino51 <shino.takeshita1124@gmail.com>
2024-01-01 16:23:43 +0100

    #70 TestCaseを継承するために、default constructorを実装するやり方をやってみた。なんでこんな簡単なことを思いつかなかったんだろう

--- shino/src/test/java/org/codepolaris/tdd/chapter2/TestCaseTest.java
+++ shino/src/test/java/org/codepolaris/tdd/chapter2/TestCaseTest.java
@@ -1,21 +1,26 @@
 package org.codepolaris.tdd.chapter2;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class TestCaseTest {
+class TestCaseTest extends TestCase {
 
+  private static final String TEST_NAME = "testTemplateMethod";
   private static final String TEST_METHOD_NAME = "testMethod";
 
-  private WasRun test;
+  private final WasRun test;
 
-  @BeforeEach
-  public void setUp() {
+  /**
+   * Java はUnit Test内でconstructorを呼び出すのが困難な為、
+   * default constructorを作成の上、必要な値を親クラスであるTestCaseに渡す
+   */
+  public TestCaseTest() {
+    super(TEST_NAME);
     this.test = new WasRun(TEST_METHOD_NAME);
   }
 
+  @Test
   void testSetup() {
     this.test.run();
     assertThat(test.isSetUp()).isTrue();
