7d0ef3ebfa0bf265ae77d0a8bb4a4058723272ab
rinahira <monofrontier@gmail.com>
2024-02-06 10:06:54 +0900

    第21章　数え上げ

--- rinahira/Python/xunit.py
+++ rinahira/Python/xunit.py
@@ -1,3 +1,12 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+        
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -6,10 +15,13 @@ class TestCase:
     def tearDown(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
 class WasRun(TestCase):
     def setUp(self):
@@ -18,6 +30,8 @@ class WasRun(TestCase):
     def testMethod(self):
         self.wasRun = 1
         self.log = self.log + "testMethod "
+    def testBrokenMethod(selft):
+        raise Exception
     def tearDown(self):
         self.log = self.log + "tearDown "
 
@@ -26,7 +40,18 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
-
+    
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+        
+        
 
 TestCaseTest("testTemplateMethod").run()
-
+TestCaseTest("testResult").run()
+#TestCaseTest("testFailedResult").run()
